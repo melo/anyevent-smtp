6ea3b05fa4810dd1e1e09efe6cf1c9a94455ffc5
Pedro Melo <melo@simplicidade.org>
2009-03-09 12:22:53 +0000

    Added ::Server attributes port, current_port and domain
     * port is the desired port where the server will listen to;
     * current_port is the port the server ended up on using;
     * domain is the domain name that the server identifies himself as.
    
    Usually `port` and `current_port` are one and the same, but you can use
    `port` as 0 or undef, and a random port will be used. In that case, the
    port used will be in the `current_port` attribute.
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- AnyEvent-SMTP/lib/AnyEvent/SMTP/Server.pm
+++ AnyEvent-SMTP/lib/AnyEvent/SMTP/Server.pm
@@ -4,6 +4,24 @@ use Mouse;
 use AnyEvent;
 use AnyEvent::Socket;
 
+has 'port' => (
+  isa => 'Str',
+  is  => 'ro',
+);
+
+has 'current_port' => (
+  isa => 'Num',
+  is  => 'rw',
+);
+
+has 'domain' => (
+  isa      => 'Str',
+  is       => 'rw',
+  required => 1,
+  default  => 'example.com',
+);
+
+
 
 no Mouse;
 __PACKAGE__->meta->make_immutable;
